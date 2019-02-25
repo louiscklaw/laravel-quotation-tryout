cdbc4c6be229bb1558b846e65b955a434cd45c71
logickee <logickee@gmail.com>
2019-02-25 19:11:05 +0800

    update,

--- app/quotation/app/Client.php
+++ app/quotation/app/Client.php
@@ -12,6 +12,25 @@ class Client extends Model
     protected $primaryKey = 'id';
 
     protected $fillable = [
-        'client_name','client_contact_tel'
+        'client_cat',
+        'client_name',
+        'client_cname',
+        'client_ename',
+        'client_address',
+        'client_contact',
+        'client_contact_tel',
+        'client_contact_mobile',
+        'client_contact_fax',
+        'client_contact_email',
+        'client_relatedsales',
+        'client_cr_code',
+        'client_change_time',
+        'client_owner_name',
+        'client_remark',
+        'client_remark2',
+        'client_remark3',
+        'client_last_mod_user',
+        'client_creation_time',
+        'client_last_mod_time'
     ];
 }

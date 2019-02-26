7fb7ec6a51496d9634fc1a444b49174008f49c44
logickee <logickee@gmail.com>
2019-02-26 19:23:21 +0800

    update for chinese fonts,

--- app/quotation/app/Http/Controllers/Pdf.php
+++ app/quotation/app/Http/Controllers/Pdf.php
@@ -15,11 +15,91 @@ class pdf_helloworld{
 class PdfHelper
 {
 
+
+    public function get_quotation_header($pdf)
+    {
+        // letter head
+        // top margin = 0
+        $pdf->Write(0, 'header', '', 0, 'C', true, 0, false, false, 0);
+
+    }
+
+    public function get_quotation_footer($pdf)
+    {
+        // bottom margin = -5
+        $pdf->SetY(-5);
+        $pdf->Write(0, 'footer', '', 0, 'C', true, 0, false, false, 0);
+    }
+
+    public function get_client_table($quot_id)
+    {
+
+$table=<<<EOF
+<table class="Quotation_table">
+    <tr>
+        <td style="text-align:left; width:80px">
+            Customer(客戶)
+        </td>
+        <td style="text-align:left; width:400px; border-left: 1px solid black;font-size:120%;">
+            123
+        </td>
+        <td style="text-align:center; width:100px; border-left: 1px solid black;">
+            Quotation Date:
+        </td>
+        <td style="text-align:center; width:90px; border-left: 1px solid black;">
+            abc
+        </td>
+    </tr>
+
+    <tr>
+        <td style="text-align:left; width:80px; border-top: 1px solid black;">
+            Address(地址)
+        </td>
+        <td style="text-align:left; width:400px; border-left: 1px solid black; border-top: 1px solid black; border-top: 1px soli black;">
+            321
+        </td>
+        <td style="text-align:center; width:100px; border-left: 1px solid black; border-top: 1px solid black;">
+            QuotatioRef.:
+        </td>
+        <td style="text-align:center; width:90px; border-left: 1px solid black; border-top: 1px solid black;">
+            cba
+        </td>
+    </tr>
+</table>
+EOF;
+        return $table;
+    }
+
     public function pdf_helloworld()
     {
+        // config
+        PDF::SetFont('droidsansfallbackhk', '', 12);
+
+        PDF::setHeaderCallback(
+            function($pdf)  {
+                $this->get_quotation_header($pdf);
+            }
+        );
+
+        PDF::setFooterCallback(
+            function($pdf)  {
+                $this->get_quotation_footer($pdf);
+            }
+        );
+
+        $html='';
+
+        // $html = $html+$this->get_client_table();
+
+        for($i=0; $i<999;$i++)
+        {
+            $html= $html.'helloworld html 中文字';
+
+        }
+
         PDF::SetTitle('Hello World');
         PDF::AddPage();
-        PDF::Write(0, 'Hello World');
+        PDF::Write(0, $html);
         PDF::Output('hello_world.pdf');
 
 

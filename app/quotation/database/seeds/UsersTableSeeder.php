e3b4b4a1a43037ad1f43b4a69923d9478cb0c599
logickee <logickee@gmail.com>
2019-03-05 20:02:30 +0800

    update,

--- app/quotation/database/seeds/UsersTableSeeder.php
+++ app/quotation/database/seeds/UsersTableSeeder.php
@@ -14,12 +14,14 @@ class UsersTableSeeder extends Seeder
             'name' => 'user'.$user_id,
             'email' => Str::random(10).'@gmail.com',
             'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'password' => '$2y$10$HpQSbKcfgyRy2JdqjVCzt.bZyxd5/iIXJWT2erzyeGZm3STMvGx7i',
+            'password' => bcrypt('123456'),
             'remember_token' => Str::random(10),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'disabled' => random_int(0,1)
         ]);
     }
+
     public function run()
     {
         DB::table('users')->truncate();

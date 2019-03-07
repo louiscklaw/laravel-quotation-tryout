21f39df3fbab2b1d64190c859b2062d0f9c47b06
logickee <logickee@gmail.com>
2019-03-07 20:43:43 +0800

    update,

--- app/quotation/database/seeds/UsersTableSeeder.php
+++ app/quotation/database/seeds/UsersTableSeeder.php
@@ -8,6 +8,18 @@ use Carbon\Carbon;
 
 class UsersTableSeeder extends Seeder
 {
+    public function insert_test_ac($user_id)
+    {
+        'name' => 'test',
+        'email' => Str::random(10).'@gmail.com',
+        'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'password' => bcrypt('test'),
+        'remember_token' => Str::random(10),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'disabled' => random_int(0,1)
+    }
+
     public function insert_record($user_id)
     {
         DB::table('users')->insert([

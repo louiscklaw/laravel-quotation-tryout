c193787f3b0efd05709218c4340eb19ae0893fa6
logickee <logickee@gmail.com>
2019-03-07 23:16:49 +0800

    update,

--- app/quotation/resources/views/layouts/components/float_label_input.blade.php
+++ app/quotation/resources/views/layouts/components/float_label_input.blade.php
@@ -1,7 +1,8 @@
 <div class="col-sm-12">
     <div class="form-group form-float">
         <div class="form-line">
-            {{ Form::text($slot,null ,
+            {{ Form::text($slot,
+                (isset($default) ? $default: null) ,
                 [   'class'=>'form-control',
                     (isset($input_disabled) ? 'disabled=""' : '')
                 ]

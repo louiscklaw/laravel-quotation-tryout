97c3fd22b3ff188f5029117e2c470b351c1f98f7
logickee <logickee@gmail.com>
2019-03-30 16:51:34 +0800

    updaste,

--- app/quotation/resources/views/layouts/client/edit.blade.php
+++ app/quotation/resources/views/layouts/client/edit.blade.php
@@ -4,83 +4,18 @@
 @section('content')
 <section class="content">
     <div class="container-fluid">
-        <div class="block-header">
-            <h2>basic form elements</h2>
-        </div>
 
         @if(isset($form_action) and $form_action =='edit')
+            <div class="block-header">
+                <h2>client editor</h2>
+            </div>
             {{ Form::model($record, ['method'=>'PATCH', 'action'=> [$update_controller, $record->id]]) }}
         @else
+            <div class="block-header">
+                <h2>create client</h2>
+            </div>
             {{ Form::model($record, ['method'=>'POST', 'action'=> [$store_controller, $record->id]]) }}
         @endif
-
-            @card([
-                'card_name'=>$editor_name,
-                'card_desc'=>$editor_description
-                ])
-
-                <div class="form-group" style="margin-top: 20px">
-                    {!! Form::submit('Confirm', ['class'=>'btn btn-primary']) !!}
-                </div>
-
-                <div class="row clearfix">
-
-
-
-                    <div class="col-sm-2">
-                        client contact
-                    </div>
-
-                    <div class="col-sm-4">
-                        @textInput(['form_class'=>'', 'placeholder'=>''])
-                            client_contact
-                        @endtextInput
-                    </div>
-
-                </div>
-            @endcard
-
-            @card([
-                    'card_name'=>'client name',
-                    'card_desc'=>'client name'
-                    ])
-                <div class="row clearfix">
-                    <div class="col-sm-4">
-                        @textInput(['form_class'=>'', 'placeholder'=>''])
-                            client_name
-                        @endtextInput
-                    </div>
-                    <div class="col-sm-4">
-                        @textInput(['form_class'=>'', 'placeholder'=>''])
-                            client_cname
-                        @endtextInput
-                    </div>
-                    <div class="col-sm-4">
-                        @textInput(['form_class'=>'', 'placeholder'=>''])
-                            client_ename
-                        @endtextInput
-                    </div>
-                </div>
-            @endcard
-
-            @card([
-                    'card_name'=>'client contact',
-                    'card_desc'=>'client contact'
-                    ])
-
-            @endcard
-            @card([
-                'card_name'=>'Client remarks',
-                'card_desc'=>'Client remarks'
-                ])
-                <div class="row clearfix">
-                    <div class="col-sm-12">
-                        @textarea(['form_class'=>'', 'placeholder'=>''])
-                            client_remark
-                        @endtextarea
-                    </div>
-                </div>
-            @endcard
 
         {{ Form::close() }}
 

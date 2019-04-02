90467bcb3e07272d02db3a4daf44455c93e8e121
logickee <logickee@gmail.com>
2019-04-02 14:16:30 +0800

    update login,

--- app/helloworld/resources/views/auth/login.blade.php
+++ app/helloworld/resources/views/auth/login.blade.php
@@ -1,33 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
-                        email address
-                                <input id="email" type="email"
-                                    class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
-                                    value="{{ old('email') }}" required autofocus>
+        email address
+                <input id="email" type="email"
+                    class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                    value="{{ old('email') }}" required autofocus>
 
-                        password
-                                <input id="password" type="password"
-                                    class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-                                    name="password" required>
-
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                        {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+        password
+                <input id="password" type="password"
+                    class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                    name="password" required>
 
 
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                        {{ old('remember') ? 'checked' : '' }}>
 
-                    </form>
+                    <label class="form-check-label" for="remember">
+                        {{ __('Remember Me') }}
+                    </label>
+                </div>
+
+
+                <button type="submit" class="btn btn-primary">
+                    {{ __('Login') }}
+                </button>
+
+
+                <a class="btn btn-link" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+    </form>
 @endsection

4033e6deb1c2dd1f45e9578b3ab857ab2bc6b026
logickee <logickee@gmail.com>
2019-04-02 14:12:58 +0800

    update,

---
+++ app/quotation/app/Http/Controllers/helloworldController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\testfileuploads;
+
+class helloworldController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('helloworld');
+    }
+
+    public function update(Request $request)
+    {
+
+        $path = $request->file('image')->store('image');
+
+        return $path;
+    }
+
+}

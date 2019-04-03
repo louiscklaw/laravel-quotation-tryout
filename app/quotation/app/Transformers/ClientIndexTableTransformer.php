58b52292ccad1433929030e959422af6426db881
logickee <logickee@gmail.com>
2019-04-03 13:38:28 +0800

    update transform,

--- app/quotation/app/Transformers/ClientIndexTableTransformer.php
+++ app/quotation/app/Transformers/ClientIndexTableTransformer.php
@@ -12,13 +12,6 @@ use App\Client;
 
 class ClientIndexTableTransformer extends TransformerAbstract
 {
-
-    public function transform()
-    {
-        return ['hello transform','hello transform'];
-    }
-
-
     public function get_action_link($client_id)
     {
         $view_link =route('client.show',['id'=>$client_id]);
@@ -34,4 +27,34 @@ class ClientIndexTableTransformer extends TransformerAbstract
 
         return $action_html;
     }
+
+    public function transform(Client $client_record)
+    {
+
+        var_dump($client_record);
+        die();
+
+        return [
+            // $client_id       => (string) $client_record->client_id,
+            $client_name       => (string) $client_record->client_name,
+            $client_cname       => (string) $client_record->client_cname,
+            $client_gender => (string) $client_record->client_gender,
+            // $client_desc       => (string) $client_record->client_cname,
+            $client_brithday => (string) $client_record->client_brithday,
+            $client_whatsapp => (string) $client_record->client_whatsapp,
+            $client_mobile => (string) $client_record->client_mobile,
+            $client_address => (string) $client_record->client_address,
+            $client_email => (string) $client_record->client_email,
+            $client_remarks => (string) $trunc_str.' ...',
+
+            $client_date => (string) $client_record->client_date,
+            $client_status => (string) $this->get_status_html($client_record->client_status),
+            $client_update_at => (string) $client_record->client_update_at,
+            $client_create_at => (string) $client_record->client_create_at,
+
+            'client_action' => $this->get_action_link($client_record->client_id)
+        ];
+    }
+
+
 }

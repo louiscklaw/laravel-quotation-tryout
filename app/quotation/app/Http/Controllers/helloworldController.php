32bc0c69efbb54bc6b8c5ded22e817261bef1f1f
logickee <logickee@gmail.com>
2019-04-03 21:21:36 +0800

    updaet,

--- app/quotation/app/Http/Controllers/helloworldController.php
+++ app/quotation/app/Http/Controllers/helloworldController.php
@@ -15,7 +15,7 @@ class helloworldController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**

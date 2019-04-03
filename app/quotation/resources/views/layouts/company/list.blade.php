38d2724c0fe5c310f4e7b6a6c7d3df93ea530bea
logickee <logickee@gmail.com>
2019-04-03 17:32:08 +0800

    update,

--- app/quotation/resources/views/layouts/company/list.blade.php
+++ app/quotation/resources/views/layouts/company/list.blade.php
@@ -20,28 +20,5 @@
 
 
 @push('blank_scripts_body')
-
-
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
-    <script>
-        $(document).ready( function () {
-            $('#table_id').DataTable();
-        } );
-    </script>
-
-    <!-- Autosize Plugin Js -->
-    <script src="{{asset('plugins/autosize/autosize.js')}}"></script>
-
-    <!-- Moment Plugin Js -->
-    <script src="{{asset('plugins/momentjs/moment.js')}}"></script>
-
-    <!-- Bootstrap Material Datetime Picker Plugin Js -->
-    <script src="{{asset('plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js')}}"></script>
-
-    <!-- Bootstrap Datepicker Plugin Js -->
-    <script src="{{asset('plugins/bootstrap-datepicker/js/bootstrap-datepicker.js')}}"></script>
-
-    <!-- Custom Js -->
-    <script src="{{asset('js/pages/forms/basic-form-elements.js')}}"></script>
-
+    @include('layouts.js_datatable')
 @endpush

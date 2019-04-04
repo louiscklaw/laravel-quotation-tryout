e5ae824d8cac3eb39f00f020f5dd067fcda00278
logickee <logickee@gmail.com>
2019-04-04 13:38:52 +0800

    update,

--- app/quotation/resources/views/layouts/quot/edit.blade.php
+++ app/quotation/resources/views/layouts/quot/edit.blade.php
@@ -66,6 +66,18 @@
                     </div>
 
 
+                </div>
+            </div>
+        <!-- col end -->
+        </div>
+    </div>
+
+
+    <div class="row clearfix">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+            <div class="card">
+                <div class="body">
                     <div class="row clearfix">
                         <div class="col-sm-12">
                             <div class="form-group">
@@ -76,10 +88,9 @@
                             </div>
                         </div>
                     </div>
-
-
                 </div>
             </div>
+
 
         <!-- col end -->
         </div>

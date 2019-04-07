2f154633457da36a792d1860304ca0891fc6e719
logickee <logickee@gmail.com>
2019-04-07 14:57:13 +0800

    update textInput,

--- app/quotation/resources/views/layouts/components/textInput.blade.php
+++ app/quotation/resources/views/layouts/components/textInput.blade.php
@@ -2,6 +2,14 @@
 <div class="form-group">
     <div class="form-line">
         {{Form::text($textinput_name, $slot,['class'=>'form-control'])}}
-        <label class="form-label">{{ __($textinput_label) }}</label>
+
+        <label class="form-label">
+            @if (isset($textinput_label))
+                {{ __($textinput_label)}}
+            @else
+                {{ __($textinput_name)}}
+            @endif
+        </label>
+
     </div>
 </div>

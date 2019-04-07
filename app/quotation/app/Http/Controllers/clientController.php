299b89ca654553bcbd669dc7ea50582b15099561
logickee <logickee@gmail.com>
2019-04-07 12:59:06 +0800

    udpate mn_highlight,

--- app/quotation/app/Http/Controllers/clientController.php
+++ app/quotation/app/Http/Controllers/clientController.php
@@ -149,7 +149,7 @@ class clientController extends Controller
             'editor_description' => 'client debug edit description',
             'update_controller' =>'clientController@update',
             'store_controller' => 'clientController@store',
-            'mn_highlight' => 'client_list'
+            'mn_highlight' => 'customer_client_new'
             ]);
     }
 

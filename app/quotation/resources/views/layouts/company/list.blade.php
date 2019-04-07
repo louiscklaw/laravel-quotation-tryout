4a1364049caff36d47d9e25f95dc11fda7840d56
logickee <logickee@gmail.com>
2019-04-07 13:21:58 +0800

    update carf token,

--- app/quotation/resources/views/layouts/company/list.blade.php
+++ app/quotation/resources/views/layouts/company/list.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.material.html')
 
+@push('append_meta')
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+@endpush
+
 @push('append_css')
     <link href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
     <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
@@ -14,7 +18,7 @@
                     <div class="card">
                         <div class="header">
                             <h2>
-                                Current Client
+                                Current Company
                             </h2>
                             <ul class="header-dropdown m-r--5">
                                 <li class="dropdown">

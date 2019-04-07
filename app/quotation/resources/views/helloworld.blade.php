83db62795ad3402a1f4a9a339f4e5f8889aa6801
logickee <logickee@gmail.com>
2019-04-07 19:29:09 +0800

    helloworld working,

--- app/quotation/resources/views/helloworld.blade.php
+++ app/quotation/resources/views/helloworld.blade.php
@@ -1,29 +1,65 @@
-@extends('layouts.material.html')
-
-@push('append_meta')
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-@endpush
-
-@push('append_head')
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- select2 -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
-@endpush
 
-@section('content')
-<section class="content">
-    <div class="row clearfix">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="card">
-                <div class="body">
-                    @bootstrap_select_ajax([
-                        'select_name'=>'select_test',
-                        'ajax_url'=> '/quotation/bs_select/client/customer_name',
-                        'preselect'=>['value'=>'ipsum@Quisqueac.net', 'text'=>'Lacy', 'subtext'=>'Lacy']
-                        ])
-                    @endbootstrap_select_ajax
-                </div>
-            </div>
+    <!-- select2-bootstrap4-theme -->
+    <link href="{{ asset('/css/select2-bootstrap4.min.css') }}" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container mt-5">
+      <h3>Example of <a href="https://github.com/ttskch/select2-bootstrap4-theme" target="_blank">@ttskch/select2-bootstrap4-theme</a></h3>
+      <hr>
+
+      <form>
+        <div class="form-group">
+          <label>Example select</label>
+          <select class="form-control" id="select-single" placeholder="Choose one thing" data-allow-clear="1">
+            <option></option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
         </div>
+        <div class="form-group">
+          <label>Example multiple select</label>
+          <select multiple class="form-control" placeholder="Choose anything" data-allow-clear="1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+      </form>
     </div>
-</section>
-@endsection
+
+    <!-- bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- select2 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+
+    <!-- select2-bootstrap4-theme -->
+    <script>
+    $(function () {
+        $('select').each(function () {
+            $(this).select2({
+            theme: 'bootstrap4',
+            width: '100%',
+            placeholder: $(this).attr('placeholder'),
+            allowClear: Boolean($(this).data('allow-clear')),
+            });
+        });
+        });
+    </script>
+  </body>
+</html>

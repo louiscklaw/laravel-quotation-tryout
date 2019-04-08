e8de044e28a3dd52abdc1bc822b6a2dceef9a4f5
logickee <logickee@gmail.com>
2019-04-08 14:47:24 +0800

    update, working datepicker,

--- app/quotation/resources/views/helloworld.blade.php
+++ app/quotation/resources/views/helloworld.blade.php
@@ -1,59 +1,28 @@
 @extends('layouts.material.html')
 
 @push('append_head')
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
-    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+    <link href="{{ asset('/plugins/bootstrap-datepicker/css/bootstrap-datepicker.css') }}" rel="stylesheet" />
 @endpush
 
+
 @section('content')
-    <section class="content">
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="header">
-                        <h2>Report bugs</h2>
-                        <small>report system abnormality</small>
+<section class="content">
+    <div class="container-fluid">
+        <div class="block-header">
+            <h2>BLANK PAGE</h2>
+            <input type="text" class="form-control">
 
-                        @button_save
-                        @endbutton_save
-
-                    </div>
-                    <div class="body">
-                        <div class="row clearfix">
-                            <div class="form-group">
-
-                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-3">
-
-                                </div>
-
-                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-3">
-                                    @select2_ajax([
-                                        'label'=>'test select2',
-                                        'placeholder' => __('test placeholder'),
-                                        'ajax_url' => 'https://api.github.com/search/repositories'
-                                    ])
-                                    @endselect2_ajax
-                                </div>
-
-                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-3">
-
-                                </div>
-                                <div class="row clearfix">
-
-                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                        @textareaMCE([
-                                            'textinput_name'=>__('bugs_report_description'),
-                                        ])
-                                        {{123}}
-                                        {{-- {{ $bugs_report->bugsreport_description }} --}}
-                                        @endtextareaMCE
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
+
+@push('blank_scripts_body')
+    <script src="{{ asset('/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js') }}"></script>
+    <script>
+        $('document').ready(function () {
+            $('input').datepicker({});
+
+        });
+    </script>
+@endpush

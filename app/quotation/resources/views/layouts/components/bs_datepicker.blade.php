0fcca8ae3e07caa02c4b748eb14a7a875db6cd79
logickee <logickee@gmail.com>
2019-04-08 18:55:58 +0800

    pre-delete commit,

--- app/quotation/resources/views/layouts/components/bs_datepicker.blade.php
+++ app/quotation/resources/views/layouts/components/bs_datepicker.blade.php
@@ -11,34 +11,30 @@
 
 @push('append_head')
 
-<!-- Bootstrap Material Datetime Picker Css -->
-<link
-    href="http://localhost:8001/quotation/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css"
-    rel="stylesheet" />
+    <!-- Bootstrap DatePicker Css -->
+    <link href="http://localhost:8001/quotation/plugins/bootstrap-datepicker/css/bootstrap-datepicker.css" rel="stylesheet" />
 
-<!-- Bootstrap DatePicker Css -->
-<link href="http://localhost:8001/quotation/plugins/bootstrap-datepicker/css/bootstrap-datepicker.css"
-    rel="stylesheet" />
+<!-- Bootstrap Material Datetime Picker Css -->
+<!-- <link href="http://localhost:8001/quotation/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css" rel="stylesheet" /> -->
 
 @endpush
 
 
 @push('append_body')
+    <!-- Moment Plugin Js -->
+    <!-- <script src="http://localhost:8001/quotation/plugins/momentjs/moment.js"></script> -->
+
+    <!-- Bootstrap Datepicker Plugin Js -->
+    <script src="http://localhost:8001/quotation/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
 
 <!-- Autosize Plugin Js -->
 <!-- <script src="http://localhost:8001/quotation/plugins/autosize/autosize.js"></script> -->
 
-<!-- Moment Plugin Js -->
-<script src="http://localhost:8001/quotation/plugins/momentjs/moment.js"></script>
-
 <!-- Bootstrap Material Datetime Picker Plugin Js -->
-<script src="http://localhost:8001/quotation/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js"> </script>
-
-<!-- Bootstrap Datepicker Plugin Js -->
-<script src="http://localhost:8001/quotation/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+<!-- <script src="http://localhost:8001/quotation/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js"> </script> -->
 
 <!-- Custom Js -->
-<script src="http://localhost:8001/quotation/js/admin.js"></script>
+<!-- <script src="http://localhost:8001/quotation/js/admin.js"></script> -->
 <!-- <script src="http://localhost:8001/quotation/js/pages/forms/basic-form-elements.js"></script> -->
 
 <!-- Demo Js -->

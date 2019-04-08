41e1ffb92ae65775a99fda16a6031cf523832619
logickee <logickee@gmail.com>
2019-04-08 18:48:56 +0800

    update,

--- app/quotation/resources/views/helloworld.blade.php
+++ app/quotation/resources/views/helloworld.blade.php
@@ -33,35 +33,8 @@
                     <div class="body">
                         <div class="row clearfix">
                             <div class="col-xs-3">
-                                <h2 class="card-inside-title">Text Input</h2>
-                                <div class="form-group">
-                                    <div class="form-line" id="bs_datepicker_container">
-                                        <input type="text" class="form-control" placeholder="Please choose a date...">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xs-3">
-                                <h2 class="card-inside-title">Component</h2>
-                                <div class="input-group date" id="bs_datepicker_component_container">
-                                    <div class="form-line">
-                                        <input type="text" class="form-control" placeholder="Please choose a date...">
-                                    </div>
-                                    <span class="input-group-addon">
-                                        <i class="material-icons">date_range</i>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="col-xs-6">
-                                <h2 class="card-inside-title">Range</h2>
-                                <div class="input-daterange input-group" id="bs_datepicker_range_container">
-                                    <div class="form-line">
-                                        <input type="text" class="form-control" placeholder="Date start...">
-                                    </div>
-                                    <span class="input-group-addon">to</span>
-                                    <div class="form-line">
-                                        <input type="text" class="form-control" placeholder="Date end...">
-                                    </div>
-                                </div>
+                                @bs_datepicker
+                                @endbs_datepicker
                             </div>
                         </div>
                     </div>
--- app/quotation/resources/views/layouts/components/bs_datepicker.blade.php
+++ app/quotation/resources/views/layouts/components/bs_datepicker.blade.php
@@ -1,10 +1,48 @@
 
-<input
-    @if (isset($name))
-        name="{{ $name }}"
-    @endif
-    type="text" class="form-control
-    @if (isset($class))
-        {{$class}}
-    @endif
-    ">
+    <h2 class="card-inside-title">Component</h2>
+    <div class="input-group date" id="bs_datepicker_component_container">
+        <div class="form-line">
+            <input type="text" class="form-control" placeholder="Please choose a date...">
+        </div>
+        <span class="input-group-addon">
+            <i class="material-icons">date_range</i>
+        </span>
+    </div>
+
+@push('append_head')
+
+<!-- Bootstrap Material Datetime Picker Css -->
+<link
+    href="http://localhost:8001/quotation/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css"
+    rel="stylesheet" />
+
+<!-- Bootstrap DatePicker Css -->
+<link href="http://localhost:8001/quotation/plugins/bootstrap-datepicker/css/bootstrap-datepicker.css"
+    rel="stylesheet" />
+
+@endpush
+
+
+@push('append_body')
+
+<!-- Autosize Plugin Js -->
+<script src="http://localhost:8001/quotation/plugins/autosize/autosize.js"></script>
+
+<!-- Moment Plugin Js -->
+<script src="http://localhost:8001/quotation/plugins/momentjs/moment.js"></script>
+
+<!-- Bootstrap Material Datetime Picker Plugin Js -->
+<script
+    src="http://localhost:8001/quotation/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js">
+</script>
+
+<!-- Bootstrap Datepicker Plugin Js -->
+<script src="http://localhost:8001/quotation/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+
+<!-- Custom Js -->
+<script src="http://localhost:8001/quotation/js/admin.js"></script>
+<script src="http://localhost:8001/quotation/js/pages/forms/basic-form-elements.js"></script>
+
+<!-- Demo Js -->
+<script src="http://localhost:8001/quotation/js/demo.js"></script>
+@endpush

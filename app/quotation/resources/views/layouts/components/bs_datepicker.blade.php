2b96cdca9554851a0b575d8d812c095099ca451a
logickee <logickee@gmail.com>
2019-04-08 18:36:15 +0800

    udpate, working by hardcode,

--- app/quotation/resources/views/layouts/components/bs_datepicker.blade.php
+++ app/quotation/resources/views/layouts/components/bs_datepicker.blade.php
@@ -1,12 +1,4 @@
 
-@push('append_head')
-    <!-- Bootstrap DatePicker Css -->
-    <link href="{{ asset('/plugins/bootstrap-datepicker/css/bootstrap-datepicker.css') }}" rel="stylesheet" />
-
-    <!-- Bootstrap Material Datetime Picker Css -->
-    <!-- <link href="{{ asset('/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css') }}" rel="stylesheet" /> -->
-@endpush
-
 <input
     @if (isset($name))
         name="{{ $name }}"
@@ -16,25 +8,3 @@
         {{$class}}
     @endif
     ">
-
-@push('append_body')
-    <!-- <script src="{{ asset('/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js') }}"></script> -->
-
-    <!-- Moment Plugin Js -->
-    <script src="{{ asset('/plugins/momentjs/moment.js')}}"></script>
-
-
-    <!-- Bootstrap Material Datetime Picker Plugin Js -->
-    <!-- <script src="{{ asset('/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js')}}"></script> -->
-
-    <!-- Bootstrap Datepicker Plugin Js -->
-    <script src="{{ asset('/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js')}}"></script>
-
-    <script>
-        $('document').ready(function () {
-            $('#bs_datepicker_component_container').datepicker({
-                orientation: "bottom auto",
-            });
-        });
-    </script>
-@endpush
